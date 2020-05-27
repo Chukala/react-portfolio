12e54c92a9ca59aa453c56253641a59c83396f45
Sileshi <sileshic@gmail.com>
2020-05-27 11:37:36 +0200

    Updates

---
+++ precache-manifest.d352456fdcc068c7d1a10af9c4b88276.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93d0e7b24cfca6f85458d180e3f95257",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "1f670756c5eaf4f1bdc7",
+    "url": "/react-portfolio/static/css/2.559fd73f.chunk.css"
+  },
+  {
+    "revision": "06cf43d36abc1da89014",
+    "url": "/react-portfolio/static/css/main.2ceee253.chunk.css"
+  },
+  {
+    "revision": "1f670756c5eaf4f1bdc7",
+    "url": "/react-portfolio/static/js/2.af4c71eb.chunk.js"
+  },
+  {
+    "revision": "c7abd4a823ee85afac2f05677368b7de",
+    "url": "/react-portfolio/static/js/2.af4c71eb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "06cf43d36abc1da89014",
+    "url": "/react-portfolio/static/js/main.09f5fc61.chunk.js"
+  },
+  {
+    "revision": "6b0ca6b1be8d47470eb8",
+    "url": "/react-portfolio/static/js/runtime-main.45267219.js"
+  },
+  {
+    "revision": "b776de207d47ccdff373e58b8ea7860d",
+    "url": "/react-portfolio/static/media/dreamShop_1.b776de20.png"
+  },
+  {
+    "revision": "da8ef97b29b6a77ac7bb04faa30830df",
+    "url": "/react-portfolio/static/media/fragbite.da8ef97b.png"
+  },
+  {
+    "revision": "369768c571952a24b3fe903483bfb118",
+    "url": "/react-portfolio/static/media/googleMerchandise.369768c5.png"
+  },
+  {
+    "revision": "c3566f895cbe8652f56dd40409e9e7c6",
+    "url": "/react-portfolio/static/media/grocery-store.c3566f89.png"
+  },
+  {
+    "revision": "09b485c730b152397941a96adc098207",
+    "url": "/react-portfolio/static/media/image_small.09b485c7.png"
+  },
+  {
+    "revision": "8508ce1c32728d9c998e1fd1402d52e9",
+    "url": "/react-portfolio/static/media/pl_project.8508ce1c.png"
+  },
+  {
+    "revision": "46f04a476f5d21c51c22aa23f39e2d61",
+    "url": "/react-portfolio/static/media/portfolio.46f04a47.png"
+  },
+  {
+    "revision": "6c84861dc49293cd054304ea70486b5e",
+    "url": "/react-portfolio/static/media/prestashop.6c84861d.png"
+  },
+  {
+    "revision": "3042d61c2acac2a3b78345ed570eadee",
+    "url": "/react-portfolio/static/media/product_landing.3042d61c.png"
+  },
+  {
+    "revision": "3fa002086ad1459c54e090d551fb0e2c",
+    "url": "/react-portfolio/static/media/randomQuote.3fa00208.png"
+  },
+  {
+    "revision": "1ee23a394f5019fd413ef67330bca7f0",
+    "url": "/react-portfolio/static/media/react-portfolio.1ee23a39.png"
+  },
+  {
+    "revision": "cfa3b2eb3751528d8d44d899d60af331",
+    "url": "/react-portfolio/static/media/screencapture_11.cfa3b2eb.png"
+  },
+  {
+    "revision": "05ec21f4604b4d7e148a9b4b06dd3e13",
+    "url": "/react-portfolio/static/media/stadium_project.05ec21f4.png"
+  },
+  {
+    "revision": "aef2fe6fb840562cc798050cfc0ec936",
+    "url": "/react-portfolio/static/media/tourguide.aef2fe6f.png"
+  }
+]);
